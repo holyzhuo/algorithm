7784e5573e8dbb8a148f553a15f1e484427edb57
zcer <1391656358@qq.com>
2019-07-30 11:37:17 +0800

    optimize

--- sort/bubble/main.go
+++ sort/bubble/main.go
@@ -1,13 +1,17 @@
 package main
 
 func SortBy(data [9]int) [9]int {
-    for j := 0; j < len(data); j++ {
-        for i := 0; i < len(data) - j - 1; i++  {
-            if data[i] > data[i+1] {
-                data[i], data[i+1] = data[i+1], data[i]
-            }
-        }
-    }
-    return data
-}
+	length := len(data)
+	if length == 0 {
+		return data
+	}
 
+	for j := 0; j < length-1; j++ {
+		for i := 0; i < length-j-1; i++ {
+			if data[i] > data[i+1] {
+				data[i], data[i+1] = data[i+1], data[i]
+			}
+		}
+	}
+	return data
+}
--- sort/bubble/main_test.go
+++ sort/bubble/main_test.go
@@ -6,14 +6,12 @@ import (
 )
 
 var TestData = map[string][9]int{
-	"1": {9,8,1,5,4,7,6,2,3},
-	"2": {1,8,7,5,4,3,2,9,6},
+	"1": {9, 8, 1, 5, 4, 7, 6, 2, 3},
+	"2": {1, 8, 7, 5, 4, 3, 2, 9, 6},
 }
 
-
 func TestBubbleSort(t *testing.T) {
-	tr := [9]int{1,2,3,4,5,6,7,8,9}
-
+	tr := [9]int{1, 2, 3, 4, 5, 6, 7, 8, 9}
 
 	for _, v := range TestData {
 		res := SortBy(v)
